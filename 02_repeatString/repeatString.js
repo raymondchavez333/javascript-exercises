58357858c5f75a8eda5f9db882b9e56f85dec309
Raymond Chavez <94435919+raymondchavez333@users.noreply.github.com>
2024-11-26 18:49:19 +0800

    Upload to github

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = function(word, times) {
+    let string="";
+    if (times<0){
+        return 'ERROR';
+    }else{
+        for(let i=0; i<times; i++){
+            string += word;
+        }
+        return string;
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
